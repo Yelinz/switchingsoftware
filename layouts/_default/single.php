59c2eb7357402ce1f0528725aec4b232ab9f3f0d
brickup <info@brickup.de>
2020-04-05 18:26:30 +0200

    Add PHP form template

--- layouts/_default/single.php
+++ layouts/_default/single.php
@@ -1,7 +1,36 @@
-<!DOCTYPE html>
-<html lang="{{ .Site.Language.Lang }}">
-<title>{{ .Title }}</title>
-<body>
-    <?php echo "{{ .Content }}"; ?>
-</body>
-</html>
+{{ define "php-header" -}}
+<?php
+    /* Load HazzelForm library */
+    $hugo_path = '{{ .File.Dir }}';
+    $hazzel_path = substring(__DIR__, 0, strlen(__DIR__) - strlen($hugo_path)) . "/submodules/HazzelForms/src/HazzelForms/HazzelForm.php";
+    require_once $hazzel_path;
+?>
+{{ end }}
+
+{{ define "main" }}
+    {{ with .Content }}
+        <section id="intro">
+            {{ . }}
+        </section>
+    {{ end }}
+
+    <?php
+        {{ with .Params.fields }}
+            $form = new HazzelForms\HazzelForm();
+            $honeypots = ['Fullname', 'Phone', 'Mail', 'Subject', 'Website'];
+            $counter = 1;
+
+            {{ range . }}
+                $form->addField('{{ .name }}', '{{ .type }}', ['placeholder' => '{{ .placeholder | default .name }}']);
+                $form->addField($honeypots[$counter++], 'honeypot');
+            {{ end }}
+
+
+            if ($form->validate()) {
+                $form->sendMail(getenv('SWISO_CONTACT_EMAIL'));
+            }
+
+            $form->renderAll();
+        {{ end }}
+    ?>
+{{ end }}
