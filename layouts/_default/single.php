7a1a65706a9eddfb66d60eea2b0dcd356364e20e
brickup <info@brickup.de>
2020-04-11 17:44:19 +0200

    Improve styling and CSP of contact form

--- layouts/_default/single.php
+++ layouts/_default/single.php
@@ -5,34 +5,59 @@
     $hazzel_path = substr(__DIR__, 0, strlen(__DIR__) - strlen($hugo_path)) . "/submodules/HazzelForms/src/HazzelForms/HazzelForm.php";
     require_once $hazzel_path;
 
-    {{ with .Params.fields }}
+    /* Init form and honeypot fields */
+    {{- with .Params.fields }}
         $form = new HazzelForms\HazzelForm();
         $honeypots = ['Fullname', 'Phone', 'Mail', 'Subject', 'Website'];
         $counter = 1;
 
-        {{ range . }}
+    /* Create form fields */
+        {{- range . }}
             $form->addField('{{ .name }}', '{{ .type }}', [
                 'label'       => '{{ .name }}',
                 'placeholder' => '{{ .placeholder | default .name }}',
                 'classlist'   => 'form-input' ]);
-            $form->addField($honeypots[$counter++], 'honeypot');
-        {{ end }}
+            $form->addField($honeypots[$counter++], 'honeypot', [
+                'classlist'   => 'd-none',
+                'inline-css'  => false ]);
+        {{- end }}
 
+    /* Validate and send form */
         if ($form->validate()) {
             $to = getenv('SWISO_CONTACT_EMAIL');
             $from = getenv('SWISO_SENDER_EMAIL');
             $form->sendMail($to, $from);
         }
-    {{ end }}
+    {{- end }}
 ?>
 {{ end }}
 
 {{ define "main" }}
-    {{ with .Content }}
+    {{- with .Content }}
         <section id="intro">
             {{ . }}
         </section>
-    {{ end }}
+    {{- end }}
 
-    <?php $form->renderAll(); ?>
+    {{- with .Params.fields }}
+    <?php
+        ob_start();
+
+        $form->openForm();
+        $counter = 1;
+        {{- range . }}
+            $form->renderField('{{ .name }}');
+            $form->renderField($honeypots[$counter++]);
+        {{- end }}
+        $form->renderSubmitErrors();
+        $form->renderSubmit();
+        $form->closeForm();
+
+        $content = ob_get_clean();
+        $content = str_replace( 'field-wrap', 'form-group', $content );
+        $content = str_replace( 'label', 'form-label', $content );
+        $content = str_replace( 'type="submit"', 'class="btn btn-primary p-centered" type="submit"', $content );
+        echo $content;
+    ?>
+    {{- end }}
 {{ end }}
